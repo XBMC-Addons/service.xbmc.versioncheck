56c41092ee0245063e5ded32a2e47a99d75c6095
Martijn Kaijser <mcm.kaijser@gmail.com>
2013-04-14 15:32:00 +0200

    move version compare to seperate file

--- service.py
+++ service.py
@@ -55,79 +55,15 @@ class Main:
                 
 def _versioncheck():
     # initial vars
-    oldversion = False
-    msg = ''
     from lib.json import get_installedversion, get_versionfilelist
+    from lib.versions import compare_version
     # retrieve versionlists from supplied version file
-    version_query = get_versionfilelist()
-    # Create seperate version lists
-    versionlist_stable = version_query['releases']['stable']
-    versionlist_rc = version_query['releases']['releasecandidate']
-    versionlist_beta = version_query['releases']['beta']
-    versionlist_alpha = version_query['releases']['alpha']
-    versionlist_prealpha = version_query['releases']['prealpha']        
+    versionlist = get_versionfilelist()
+    # retrieve version installed
     version_installed = get_installedversion()
-
-    ### Check to upgrade to newest available stable version
-    # check on smaller major version. Smaller version than available always notify
-    if version_installed['major'] < int(versionlist_stable[0]['major']):
-        msg = __localize__(32003)
-        oldversion = True
-        log("Version available  %s" %versionlist_stable[0])
-
-    # check on same major version installed and available
-    elif version_installed['major'] == int(versionlist_stable[0]['major']):
-        # check on smaller minor version
-        if version_installed['minor'] < int(versionlist_stable[0]['minor']):
-            msg = __localize__(32003)
-            oldversion = True
-            log("Version available  %s" %versionlist_stable[0])
-        # check if not installed a stable so always notify
-        elif version_installed['minor'] == int(versionlist_stable[0]['minor']) and version_installed['tag'] != "stable":
-            msg = __localize__(32008)
-            oldversion = True
-            log("Version available  %s" %versionlist_stable[0])
-        else:
-            log("Last available stable installed")
-
-    ### Check to upgrade to newest available RC version if not installed stable
-    ## Check also oldversion hasn't been set true by previous check because if so this need to be skipped
-    if not oldversion and version_installed['tag'] != "stable":
-        # only check on equal or lower major because newer installed beta/alpha/prealpha version will be higher
-        if versionlist_rc and version_installed['major'] <= int(versionlist_rc[0]['major']):
-            if version_installed['revision'] <= versionlist_rc[0]['revision']:
-                msg = __localize__(32004)
-                oldversion = True
-                log("Version available  %s" %versionlist_rc[0])
-
-        # exclude if installed RC on checking for newer beta
-        if not oldversion and versionlist_beta and version_installed['tag'] not in ["releasecandidate"]:
-            if version_installed['major'] <= int(versionlist_beta[0]['major']):
-                if version_installed['revision'] < versionlist_beta[0]['revision']:
-                    msg = __localize__(32005)
-                    oldversion = True
-                    log("Version available  %s" %versionlist_beta[0])
-    
-        # exclude if installed RC or beta on checking for newer alpha
-        if not oldversion and versionlist_alpha and version_installed['tag'] not in ["releasecandidate", "beta"]:
-            if version_installed['major'] <= int(versionlist_alpha[0]['major']):
-                if version_installed['revision'] < versionlist_alpha[0]['revision']:
-                    msg = __localize__(32006)
-                    oldversion = True
-                    log("Version available  %s" %versionlist_alpha[0])
-
-        # exclude if installed RC, beta or alpha on checking for newer prealpha
-        if not oldversion and versionlist_prealpha and version_installed['tag'] not in ["releasecandidate", "beta", "alpha"]:
-            if version_installed['major'] <= int(versionlist_prealpha[0]['major']):
-                if version_installed['revision'] < versionlist_prealpha[0]['revision']:
-                    msg = __localize__(32007)
-                    oldversion = True
-                    log("Version available  %s" %versionlist_prealpha[0])
-
-        # Nothing to see here, move along
-    else:
-        # Nothing to see here, move along
-        pass
+    # copmpare installed and available
+    oldversion, msg = compare_version(version_installed, versionlist)
+    msg = __localize__(msg)
     return oldversion, msg
 
 
