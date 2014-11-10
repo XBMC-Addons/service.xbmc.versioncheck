XBMC-Addons/service.xbmc.versioncheck

commit 7cc88aff2eecfe875951cc689824c96ae8e0e2d1

[service.xbmc.versioncheck] 0.1.18

service.py
@@ -17,7 +17,6 @@
 #    along with this program. If not, see <http://www.gnu.org/licenses/>.
 #
 
-
 import platform
 import xbmc
 import lib.common
@@ -29,12 +28,13 @@ __addonversion__ = lib.common.__addonversion__
 __addonname__    = lib.common.__addonname__
 __addonpath__    = lib.common.__addonpath__
 __icon__         = lib.common.__icon__
+oldversion = False
 
 class Main:
     def __init__(self):
         linux = False
         packages = []
-        if __addon__.getSetting("versioncheck_enable") == 'true' and not xbmc.getCondVisibility('System.HasAddon(os.openelec.tv)'):
+        if not xbmc.getCondVisibility('System.HasAddon(os.openelec.tv)'):
             if not sys.argv[0]:
                 xbmc.executebuiltin('XBMC.AlarmClock(CheckAtBoot,XBMC.RunScript(service.xbmc.versioncheck, started),00:00:30,silent)')
                 xbmc.executebuiltin('XBMC.AlarmClock(CheckWhileRunning,XBMC.RunScript(service.xbmc.versioncheck, started),24:00:00,silent,loop)')
@@ -45,7 +45,7 @@ class Main:
                 else:
                     oldversion, msg = _versioncheck()
                     if oldversion:
-                        _upgrademessage(msg, False)
+                        _upgrademessage(msg, oldversion, False)
             else:
                 pass
                 
@@ -85,7 +85,7 @@ def _versionchecklinux(packages):
 
         if handler:
             if handler.check_upgrade_available(packages[0]):
-                if _upgrademessage(32012, True):
+                if _upgrademessage(32012, oldversion, True):
                     if __addon__.getSetting("upgrade_system") == "false":
                         result = handler.upgrade_package(packages[0])
                     else:

addon.xml
@@ -1,7 +1,7 @@
 <?xml version="1.0" encoding="UTF-8" standalone="yes"?>
 <addon id="service.xbmc.versioncheck"
        name="XBMC Version Check"
-      version="0.1.17"
+      version="0.1.18"
       provider-name="Team XBMC">
     <requires>
         <import addon="xbmc.addon" version="12.0.0"/>

lib/common.py
@@ -16,6 +16,7 @@
 #    along with this program. If not, see <http://www.gnu.org/licenses/>.
 #
 
+import os
 import xbmc
 import xbmcaddon
 import xbmcgui
@@ -25,6 +26,7 @@ __addon__        = xbmcaddon.Addon()
 __addonversion__ = __addon__.getAddonInfo('version')
 __addonname__    = __addon__.getAddonInfo('name')
 __addonpath__    = __addon__.getAddonInfo('path').decode('utf-8')
+__addonprofile__ = xbmc.translatePath( __addon__.getAddonInfo('profile') ).decode('utf-8')
 __icon__         = __addon__.getAddonInfo('icon')
 
 def localise(id):
@@ -59,7 +61,7 @@ def dialog_yesno(line1 = 0, line2 = 0):
                                   localise(line1),
                                   localise(line2))
 
-def upgrade_message(msg, upgrade):
+def upgrade_message(msg, oldversion, upgrade):
     # Don't show while watching a video
     while(xbmc.Player().isPlayingVideo() and not xbmc.abortRequested):
         xbmc.sleep(1000)
@@ -75,12 +77,13 @@ def upgrade_message(msg, upgrade):
                             localise(32001),
                             localise(32002))
         # sets check to false which is checked on startup
-        if dialog_yesno(32009, 32010):
+        if oldversion == "stable" and dialog_yesno(32009, 32010):
             __addon__.setSetting("versioncheck_enable", 'false')
         # set first run to false to only show a popup next startup / every two days
         __addon__.setSetting("versioncheck_firstrun", 'false')
+
     # Show notification after firstrun
-    elif not xbmc.abortRequested:
+    elif __addon__.getSetting("versioncheck_enable") == 'true':
         if upgrade:
             return dialog_yesno(msg)
         else:
@@ -90,3 +93,10 @@ def upgrade_message(msg, upgrade):
                                                                   __icon__))
     else:
         pass
+    # tells users to upgrade to nightlies as there are no more monthlies
+    if oldversion == "monthly":
+        upgrademessage = __addon__.getSetting("versioncheck_gotham_alpha_notice") != 'false'
+        if upgrademessage:
+            __addon__.setSetting("versioncheck_gotham_alpha_notice", 'false')
+            runcmd = os.path.join(__addonpath__, 'lib/viewer.py')
+            xbmc.executebuiltin('XBMC.RunScript (%s,%s) '%(runcmd, 'gotham-alpha_notice'))

lib/versions.py
@@ -31,7 +31,7 @@ def compare_version(version_installed, versionlist):
     msg = ''
     if version_installed['major'] < int(versionlist_stable[0]['major']):
         msg = 32003
-        oldversion = True
+        oldversion = "stable"
         log("Version available  %s" %versionlist_stable[0])
 
     # check on same major version installed and available
@@ -39,12 +39,12 @@ def compare_version(version_installed, versionlist):
         # check on smaller minor version
         if version_installed['minor'] < int(versionlist_stable[0]['minor']):
             msg = 32003
-            oldversion = True
+            oldversion = "stable"
             log("Version available  %s" %versionlist_stable[0])
         # check if not installed a stable so always notify
         elif version_installed['minor'] == int(versionlist_stable[0]['minor']) and version_installed['tag'] != "stable":
             msg = 32008
-            oldversion = True
+            oldversion = "stable"
             log("Version available  %s" %versionlist_stable[0])
         else:
             log("Last available stable installed")
@@ -82,6 +82,15 @@ def compare_version(version_installed, versionlist):
                     if  version_installed['revision'] < versionlist_prealpha[0]['revision']:
                         msg = 32007
                         oldversion = True
+                        log("Version available  %s" %versionlist_prealpha[0])
+
+            # exclude if installed RC, beta or alpha on checking for newer prealpha
+            # if no more monthlies are build enable this section
+            if not oldversion and versionlist_prealpha and version_installed['tag'] not in ["releasecandidate", "beta", "alpha"]:
+                if version_installed['major'] <= int(versionlist_prealpha[0]['major']):
+                    if  version_installed['revision'] <= versionlist_prealpha[0]['revision']:
+                        msg = 32007
+                        oldversion = "monthly"
                         log("Version available  %s" %versionlist_prealpha[0])
 
         log("Nothing to see here, move along. Running a latest non stable release")

lib/viewer.py
@@ -1,0 +1,99 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+#     Copyright (C) 2011-2013 Martijn Kaijser
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+
+#import modules
+import os
+import sys
+import xbmc
+import xbmcgui
+import xbmcaddon
+
+### get addon info
+__addon__        = xbmcaddon.Addon('service.xbmc.versioncheck')
+__addonversion__ = __addon__.getAddonInfo('version')
+__addonname__    = __addon__.getAddonInfo('name')
+__addonpath__    = __addon__.getAddonInfo('path').decode('utf-8')
+__addonprofile__ = xbmc.translatePath( __addon__.getAddonInfo('profile') ).decode('utf-8')
+__icon__         = __addon__.getAddonInfo('icon')
+
+class Viewer:
+    # constants
+    WINDOW = 10147
+    CONTROL_LABEL = 1
+    CONTROL_TEXTBOX = 5
+
+    def __init__(self, *args, **kwargs):
+        # activate the text viewer window
+        xbmc.executebuiltin("ActivateWindow(%d)" % (self.WINDOW,))
+        # get window
+        self.window = xbmcgui.Window(self.WINDOW)
+        # give window time to initialize
+        xbmc.sleep(100)
+        # set controls
+        self.setControls()
+
+    def setControls(self):
+        #get header, text
+        heading, text = self.getText()
+        # set heading
+        self.window.getControl(self.CONTROL_LABEL).setLabel("%s : %s" % (__addonname__, heading, ))
+        # set text
+        self.window.getControl(self.CONTROL_TEXTBOX).setText(text)
+        xbmc.sleep(10000)
+
+    def getText(self):
+        try:
+            if sys.argv[ 1 ] == "gotham-alpha_notice":
+                return "Call to Gotham alpha users", self.readFile(os.path.join(__addonpath__ , "resources/gotham-alpha_notice.txt"))
+        except Exception, e:
+            xbmc.log(__addonname__ + ': ' + str(e), xbmc.LOGERROR)
+        return "", ""
+
+    def readFile(self, filename):
+        return open(filename).read()
+
+class WebBrowser:
+    """ Display url using the default browser. """
+
+    def __init__(self, *args, **kwargs):
+        try:
+            url = sys.argv[2]
+            # notify user
+            notification(__addonname__, url)
+            xbmc.sleep(100)
+            # launch url
+            self.launchUrl(url)
+        except Exception, e:
+            xbmc.log(__addonname__ + ': ' + str(e), xbmc.LOGERROR)
+
+    def launchUrl(self, url):
+        import webbrowser
+        webbrowser.open(url)
+
+def Main():
+    try:
+        if sys.argv[ 1 ] == "webbrowser":
+            WebBrowser()
+        else:
+            Viewer()
+    except Exception, e:
+        xbmc.log(__addonname__ + ': ' + str(e), xbmc.LOGERROR)
+
+if (__name__ == "__main__"):
+    Main()

commit 8ee6c3a72b8776e471189a70bef26803e5e63669

Check if add-on is actually enabled.
Fixes: 7cc88aff2eecfe875951cc689824c96ae8e0e2d1

service.py
@@ -35,7 +35,7 @@ class Main:
     def __init__(self):
         linux = False
         packages = []
-        if not xbmc.getCondVisibility('System.HasAddon(os.openelec.tv)'):
+        if __addon__.getSetting("versioncheck_enable") == 'true' and not xbmc.getCondVisibility('System.HasAddon(os.openelec.tv)'):
           xbmc.sleep(5000)
           if xbmc.getCondVisibility('System.Platform.Linux') and __addon__.getSetting("upgrade_apt") == 'true':
               packages = ['xbmc']