935b9014b14fdf65e0dfc9559e241b18ba054809
anxdpanic <anxdpanic@gmail.com>
2019-10-28 16:55:09 -0400

    SPDX license identifier
    - add missing GPL-3.0-or-later license file
    - update copyright notices
    - replace file license header with SPDX license identifier and license location

--- lib/__init__.py
+++ lib/__init__.py
@@ -1,1 +1,15 @@
+# -*- coding: utf-8 -*-
+
+"""
+
+    Copyright (C) 2013-2014 Team-XBMC
+    Copyright (C) 2014-2019 Team Kodi
+
+    This file is part of service.xbmc.versioncheck
+
+    SPDX-License-Identifier: GPL-3.0-or-later
+    See LICENSES/GPL-3.0-or-later.txt for more information.
+
+"""
+
 from .common import *
